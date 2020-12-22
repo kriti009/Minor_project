21740f9bb95c6f97040f9aa0cee3e0ea326c5fc9
Shritesh <shritesh.sj@gmail.com>
2020-12-22 21:31:21 +0530

    getHome function working

--- src/ContractFunc.js
+++ src/ContractFunc.js
@@ -1,7 +1,24 @@
-const contract = require('truffle-contract');
-const artifacts = require('./Blockchain/abis/SmartHome.json');
-const SmartHome = contract(artifacts);
+const Contract = require("@truffle/contract");
+const Web3 = require("web3");
+
+const abi = require("./Blockchain/abis/SmartHome.json");
+const SmartHome = Contract(abi);
+let web3;
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+	web3 = new Web3(window.web3.currentProvider);
+} else {
+	const provider = new Web3.providers.HttpProvider(
+		process.env.REACT_APP_WEB3_PROVIDER || "http://127.0.0.1:8545"
+	);
+	web3 = new Web3(provider);
+}
+SmartHome.setProvider(web3.currentProvider);
 
 module.exports = {
-    
-}
+	web3: web3,
+
+	getHome: async () => {
+		const contract = await SmartHome.deployed();
+		return await contract.getHome();
+	},
+};
